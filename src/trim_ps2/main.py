b5bec7d36ec19f0411c7e8fa4f52da4d5d22f9b9
remico <remicollab@gmail.com>
2020-11-13 00:55:00 +0200

    refactored

--- src/trim_ps2/main.py
+++ src/trim_ps2/main.py
@@ -19,137 +19,153 @@ Intended to use in conjunction with Briefcase tool.
 import subprocess
 import sys
 
+from os import getcwd
 from pathlib import Path
-
 
 E_CODE_NO_TARGET = 1
 E_CODE_TARGET_ABORT = 2
 E_CODE_NO_PS2 = 3
 
-try:
-    app_module = sys.argv[1]
-except IndexError:
-    print(f"Usage: python3 -m {__package__} <PYTHON_PACKAGE>")
-    sys.exit(E_CODE_NO_TARGET)
+
+def app_module():
+    try:
+        target = sys.argv[1]
+        return target
+    except IndexError:
+        print(f"Usage: python3 -m {__package__} <PYTHON_PACKAGE> [-d] [--dump]")
+        sys.exit(E_CODE_NO_TARGET)
 
 
-app_bundle_path = Path(sys.executable).parent.parent
-ps2_path = app_bundle_path / 'app_packages/PySide2/'
+def check_option(o: str):
+    exists = o in sys.argv
+    if exists:
+        sys.argv.remove(o)
+    return exists
 
-required_deps = set()
 
+DUMP = check_option("--dump")  # dump all required dependencies
 
-# FIXME pexpect.interact()
-# if "-d" in sys.argv:
-#     res = subprocess.run(f"{sys.executable} -m pudb {__file__} {app_module}",
-#                             cwd= str(app_bundle_path / "app" / app_module),
-#                             text=True,
-#                             shell=True,
-#                             )
+# FIXME
+# if check_option("-d"):
+#     command = f"{sys.executable} -m pudb {__file__} {app_module()}"
+#     res = pexpect.interact(...),
 #     sys.exit(res.returncode)
 
 
 def run_p(cmd, errors=False):
-    return subprocess.run(cmd,
-                          cwd = str(app_bundle_path / "app"),
-                          text=True,
-                          shell=True,
+    return subprocess.run(cmd, cwd=working_dir, text=True, shell=True,
                           stderr=subprocess.STDOUT if errors else subprocess.PIPE,
                           stdout=subprocess.PIPE)
 
 
-def find_app_deps(py_module):
-    command = f"QT_DEBUG_PLUGINS=1 {sys.executable} -v -m {py_module} 2>&1"
-
-    res = run_p(command + ' | grep --line-buffered -oP "loaded.*/.*?\.so[\.\d]*"', errors=True)
-
-    if "Abort" in res.stdout or res.returncode != 0:
+def locate_ps2(root_path):
+    ps2_dir = root_path / 'PySide2/'
+    if not ps2_dir.exists():
+        command = fr"{sys.executable} -B -v -c 'import PySide2.QtCore' 2>&1 | grep 'loaded' | grep -oP '/.*QtCore.*\.so'"
         res = run_p(command, errors=True)
+
+        if not res.stdout:
+            print(f"ERROR: PySide2 not found")
+            sys.exit(E_CODE_NO_PS2)
+
+        ps2_dir = Path(res.stdout).parent
+    return ps2_dir
+
+
+def locate_cwd(root_path):
+    app_dir = root_path / "app"
+    return str(app_dir) if app_dir.exists() else getcwd()
+
+
+app_bundle_path = Path(sys.executable).parent.parent
+app_bundle_packages_path = app_bundle_path / "app_packages"
+working_dir = locate_cwd(app_bundle_path)
+ps2_path = locate_ps2(app_bundle_packages_path)
+
+
+def find_app_deps(py_module):
+    error_msgs = [
+        'Abort',
+        'ImportError',
+        'Traceback',
+    ]
+    errors_filter = '|'.join(error_msgs)
+    libs_filer = r"/.*\.so[\.\d]*"
+    libs_import_error_filter = r"(?<=ImportError: ).*.so[\.\d]*"
+
+    command = f"QT_DEBUG_PLUGINS=1 {sys.executable} -B -v -m {py_module} 2>&1"
+    output_filter = f' | egrep "loaded|{errors_filter}" | grep -oP "{libs_filer}|{libs_import_error_filter}|{errors_filter}"'
+
+    res = run_p(command + output_filter, errors=True)
+
+    if (any(emsg in res.stdout for emsg in error_msgs) and not DUMP) or res.returncode != 0:
+        output_filter = " | egrep -v '# cleanup|# destroy'"
+        res = run_p(command + output_filter, errors=True)
         print(res.stdout)
-        print(f"\nERROR: '{app_module}' module exited with code {res.returncode}. Abort deps gathering.")
+        print(f"\nERROR: '{py_module}' module exited with code {res.returncode}. Abort deps gathering.")
         sys.exit(E_CODE_TARGET_ABORT)
 
     all_deps = res.stdout.splitlines()
+    so_libs = {lib for lib in all_deps if ".so" in lib and "python" not in Path(lib).name}
 
-    so_libs = {m.split('/')[-1] for m in all_deps}  # [!] cut filenames before filtering
-    so_libs = {m for m in so_libs if ".so" in m and "python" not in m}
-
-    print(so_libs)
-
-    required_deps.update(so_libs)
-    return required_deps
+    return so_libs
 
 
-def find_elf_deps(libs):
+def is_path(value: str):
+    return len(value.split('/')) > 1
+
+
+def find_all_elf_deps(libs):
     deps = set()
 
-    if not ps2_path.exists():
-        print(f"ERROR: app's path '{ps2_path}' doesn't exists")
-        sys.exit(E_CODE_NO_PS2)
-
     for lib in libs:
-        # collect all deps
-        res = run_p(f"ldd $(find {ps2_path} -name {lib}) | egrep -v 'linux-vdso|ld-linux'")
-        s = {l.strip().split(" => ")[0] for l in res.stdout.splitlines()}
-        s = {l.split(' ')[0] for l in s}
+        deps.add(lib)
+
+        if not is_path(lib):  # don't even try to resolve plain names
+            continue
+
+        loaded_path = r"(?<==>).*(?=\()"
+        not_found_name = r".*(?==> not found)"
+        res = run_p(f"ldd {lib} | egrep -v 'linux-vdso|ld-linux' | grep -oP '{loaded_path}|{not_found_name}'")
+        lines = res.stdout.splitlines()
+        s = {ln.strip() for ln in lines}
         deps.update(s)
 
-    print("@ Qt deps:\n", deps)
     return deps
 
 
 def dpkg_resolve(libs):
-    resolved = set()
-    unresolved= set()
+    resolved_paths = set()
+    unresolved_names_and_paths = set()
 
     for lib_path in libs:
         res = run_p(f"dpkg -S '{lib_path}'")
-        # resolved.add(res.stdout.split(': ')[0])  # + suffix :amd64
         if not res.stdout.strip() \
                 or "no path found" in res.stdout:
-            unresolved.add(lib_path)
+            unresolved_names_and_paths.add(lib_path)
         elif res.stdout.strip():
-            resolved.add(res.stdout.split(':')[0])
+            resolved_paths.add(res.stdout.split(':')[0])
         else:
-            print(f"@ WARNING: {lib_path}")
+            print(f"@ WARNING: something wrong with '{lib_path}'")
 
-    print()
-    print("@ dpkg resolved:\n", resolved)
-    print("@ still unresolved:\n", resolved)
-    return unresolved
+    return resolved_paths, unresolved_names_and_paths
 
 
 def handle_dpkg_unresolved(libs):
-    libs = list(libs)
-
-    # first look at app_dir
-    found_in_app_dir = look_at_app_dir(libs)
-    print()
-    print("@ found in APP_DIR:\n", found_in_app_dir, "\n")
-
-    for found_lib in found_in_app_dir:
-        for orig_lib in libs:
-            if found_lib in orig_lib:
-                libs.remove(orig_lib)
-
-    print()
-    print("@ total unresolved:\n", libs)
-
-    # then find remaining dependencies in OS index
     if libs:
         lib_paths = '\n'.join(libs)
         res = run_p(f"echo '{lib_paths}' | apt-file find -f -", errors=True)
 
-        packages = {l: set() for l in libs}  # initialize !
+        packages = {lib: set() for lib in libs}  # initialize
 
-        for p in res.stdout.splitlines():
-            pkg_name = p.split(": ")[0]
-            lib_suggested_name = p.split(": ")[1]
+        for pkg in res.stdout.splitlines():
+            pkg_name = pkg.split(": ")[0]
+            lib_suggested_name = pkg.split(": ")[1]
 
             def resolve_lib_name(name):
-                for l in libs:
-                    if l in name:
-                        return l
+                for lib in libs:
+                    if lib in name:
+                        return lib
                 return "#"
 
             lib_name = resolve_lib_name(lib_suggested_name)
@@ -158,64 +174,111 @@ def handle_dpkg_unresolved(libs):
         print()
         print("@ apt-file suggests:\n")
         for k, v in packages.items():
-            print(f"> {k:20}: {list(v)}")
+            print(f"> {Path(k).name:20}: {list(v)}")
 
 
-def look_at_app_dir(libs):
-    libs = [l.split('/')[-1] for l in libs]  # extract libs' filenames from path-like strings
-    found_in_app_dir = set()
+def filter_python_modules(libs):
+    modules = set()
 
-    for lib in libs:
-        res = run_p(f'find {app_bundle_path} -name "{lib}"')
-        if res.returncode == 0 and res.stdout.strip():
-            found_in_app_dir.add(lib)
+    names = {lib for lib in libs if not is_path(lib)}  # plain .so names
+    paths = {lib for lib in libs if is_path(lib)}  # .so paths
 
-    required_deps.update(found_in_app_dir)
-    return found_in_app_dir
+    for path in paths:
+        if "lib/python" in path:
+            modules.add(path)
+        elif str(app_bundle_packages_path) in path:
+            modules.add(path)
+
+        for name in names:
+            if name in path:
+                modules.add(name)
+
+    return modules
 
 
-def calculate_size():
+def to_names(paths):
+    return {Path(path).name for path in paths}
+
+
+def calculate_pyside2_size():
     return run_p(f"du -sh {ps2_path}").stdout.split('\t')[0]
 
 
 def main():
+    required_deps = set()
+
     print("\n***************** APP DEPS *******************\n")
-    app_deps = find_app_deps(app_module)
+    app_deps = find_app_deps(app_module())
+    print(to_names(app_deps))
 
     print("\n************** RECURSIVE DEPS ****************\n")
-    all_deps = find_elf_deps(app_deps)
-    unresolved_deps = dpkg_resolve(all_deps)
-    handle_dpkg_unresolved(unresolved_deps)
+    all_deps = find_all_elf_deps(app_deps)
 
-    print("\n********** REQUIRED PySide2 DEPS *************\n")
-    print(required_deps)
+    ps2_modules = {m for m in all_deps if 'PySide2' in m}
+    required_deps.update(ps2_modules)
+    other_libs = all_deps - ps2_modules
 
-    size_before = calculate_size()
+    if DUMP:
+        print("@ PySide2 modules:")
+        print(to_names(ps2_modules))
+        print()
+        print("@ other deps:")
+        print(to_names(other_libs))
 
-    print()
-    print("Clearing unused dependencies...")
-    globbed_so = ps2_path.glob("**/*.so*")
-    so_paths = [str(so) for so in globbed_so]
+        print()
+        print("Resolving dependencies...")
 
-    for so_path in so_paths:
-        so_name = str(so_path).split('/')[-1]
+        resolved, unresolved = dpkg_resolve(other_libs)
 
-        # skip required deps
-        if any(so_name in dep for dep in required_deps):
-            continue
+        if resolved:
+            print()
+            print("@ dpkg resolved packages:")
+            print("$ sudo apt install", ' '.join(resolved))
 
-        run_p(f"rm {so_path}")
+        # filter other (non-PS2) python modules
+        other_python_modules = filter_python_modules(unresolved)
+        unresolved -= other_python_modules
 
-    for name in ["designer", "rcc", "uic", "pyside2-lupdate"]:
-        run_p(f"find {ps2_path} -type f -name {name} -exec rm {{}} +")
+        print()
+        print("@ recognized as python modules:")
+        print(to_names(other_python_modules))
 
-    print("Done")
-    print()
+        print()
+        print("@ total unresolved:")
+        print(to_names(unresolved))
 
-    size_after = calculate_size()
-    print("PySide2 directory size before:", size_before)
-    print("PySide2 directory size after:", size_after)
-    print()
+        # find remaining dependencies in OS index
+        handle_dpkg_unresolved(unresolved)
+
+    print("\n********** REQUIRED PySide2 ELFs **********\n")
+    print(to_names(required_deps))
+
+    if not DUMP:
+        print()
+        print("Clearing unused dependencies...")
+
+        size_before = calculate_pyside2_size()
+
+        globbed_so = ps2_path.glob("**/*.so*")
+        so_paths = [str(so) for so in globbed_so]
+
+        for so_path in so_paths:
+            # skip required deps
+            if any(Path(so_path).name in dep_path for dep_path in required_deps):
+                continue
+
+            run_p(f"rm {so_path}")
+
+        for name in ["designer", "rcc", "uic", "pyside2-lupdate"]:
+            run_p(f"find {ps2_path} -type f -name {name} -exec rm {{}} +")
+
+        print("Done")
+        print()
+
+        size_after = calculate_pyside2_size()
+        print("PySide2 directory size before:", size_before)
+        print("PySide2 directory size after:", size_after)
+        print()
 
 
 if __name__ == '__main__':
